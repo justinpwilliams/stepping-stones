0a6432f47d2ea9a6f01a13b87eb903632dd21ff7
justinpwilliams <jpwilliams@gatech.edu>
2022-06-23 16:54:46 -0700

    loaded graph nodes and edges, found shortest paths

--- graph.py
+++ graph.py
@@ -1,6 +1,25 @@
 import networkx as nx
 import pandas as pd
+import matplotlib.pyplot as plt
 
 topics_file = "LectureBank-master/LB-Paper/208topics.csv"
+prereq_file = "LectureBank-master/LB-Paper/prerequisite_annotation.csv"
 
 
+def load_topics(filename=topics_file):
+    return pd.read_csv(filename, names=['ID', 'Name', 'Link'])
+
+
+def load_prereqs(filename=prereq_file):
+    return pd.read_csv(filename)
+
+
+def make_graph(topics, prereqs):
+    g = nx.DiGraph()
+
+    # Add Nodes
+    g.add_nodes_from([(topic[0], {"name": topic[1], "link": topic[2]}) for topic in topics.values])
+
+    # Add directed edges when prerequisite relationship is true
+    g.add_edges_from([(ids[0], ids[1]) for ids in prereqs.values if ids[2] == 1])
+    return g
---
+++ tests/graph_test.py
@@ -0,0 +1,38 @@
+import unittest
+import matplotlib.pyplot as plt
+import networkx as nx
+import graph
+
+
+class GraphTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.topics_file = "../LectureBank-master/LB-Paper/208topics.csv"
+        self.prereq_file = "../LectureBank-master/LB-Paper/prerequisite_annotation.csv"
+        self.topics = graph.load_topics(self.topics_file)
+        self.prereqs = graph.load_prereqs(self.prereq_file)
+        self.graph = graph.make_graph(self.topics, self.prereqs)
+
+    def test_can_add_directed_edges_to_graph(self):
+        self.assertTrue(16 in self.graph.successors(2))
+
+    def test_can_draw_graph(self):
+        nx.draw(self.graph, nodelist=list(self.graph.successors(2)), edgelist=[edge for edge in self.graph.edges if 2 in edge], with_labels=True)
+        # plt.show()
+        self.assertTrue(True)
+
+    def test_can_find_shortest_path(self):
+        path = nx.bidirectional_shortest_path(self.graph, 19, 59)
+        print(path)
+        self.assertLessEqual(len(path), 3)
+
+    def test_can_find_simple_shortest_paths(self):
+        simple_paths = list(nx.all_shortest_paths(self.graph, 154, 2))
+        print(simple_paths)
+        self.assertGreater(len(simple_paths), 0)
+
+    def test_can_find_shortest_path_to_target(self):
+        all_paths = nx.single_target_shortest_path(self.graph, 59)
+        self.assertGreater(len(all_paths), 0)
+
+if __name__ == '__main__':
+    unittest.main()
