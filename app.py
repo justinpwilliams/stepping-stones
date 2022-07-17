bd837185a458e5c5807771f2ac65819861db7e08
justinpwilliams <jpwilliams@gatech.edu>
2022-07-17 08:28:58 -0700

    so many changes I lost count

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from werkzeug.wrappers import Request, Response
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+
+import graph
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def index():
+    print("Home page requested")
+    return render_template("index.html")
+
+
+@app.route("/ac_test", methods=["POST", "GET"])
+def ac_test():
+    print("Concept selection page requested")
+    if request.method == "GET":
+        concepts = graph.load_topics('LectureBank-master/LB-Paper/208topics.csv')
+        concepts = list(concepts['Name'])
+
+        return render_template("ac_test.html", concepts=concepts)
 
 
 if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-
-    run_simple('localhost', 9000, app)
+    app.run()
